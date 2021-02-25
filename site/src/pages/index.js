0fd39d5b571e87c2be840139fb70a99c7c1c5ed2
Alec Neevel <aneevel15@gmail.com>
2021-02-24 22:01:53 -0600

    Edit title to include personal info

--- site/src/pages/index.js
+++ site/src/pages/index.js
@@ -1,5 +1,4 @@
 import React from "react"
-import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import Image from "../components/image"
@@ -7,15 +6,11 @@ import SEO from "../components/seo"
 
 const IndexPage = () => (
   <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+    <SEO title="Alec Neevel - Professional Web Software Engineer" />
+    <h1>Hi! My name is Alec Neevel.</h1>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
-    <Link to="/page-2/">Go to page 2</Link> <br />
-    <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
   </Layout>
 )
 
