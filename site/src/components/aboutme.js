be9710e0056ed8d0be43345b411b970a0e07a815
Alec Neevel <aneevel15@gmail.com>
2021-02-26 02:45:58 -0600

    Add Image and some content to about-me

--- site/src/components/aboutme.js
+++ site/src/components/aboutme.js
@@ -1,8 +1,17 @@
 import React from "react"
+import Image from "./image"
 
 const AboutMe = () => {
     return (
-        <div id="aboutme-container"></div>
+        <div id="aboutme-container">
+            <Image/>
+            <h2>A little about me...</h2>
+            <p>My name is Alec Neevel. I am web software engineer living in Madison, Wisconsin.
+               I like to work with the full-stack, handling anything from React (which this site is built on!)
+               to SQL queries.
+            </p>
+            <Button/>
+        </div>
     )
 }
 
