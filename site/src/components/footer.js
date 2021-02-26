ea82995d48900eb54c95973d6c970bcb2f5da7ed
Alec Neevel <aneevel15@gmail.com>
2021-02-26 02:38:19 -0600

    Move footer element to its own component

---
+++ site/src/components/footer.js
@@ -0,0 +1,17 @@
+import React from "react"
+
+const Footer = () => {
+    return (
+        <footer
+          style={{
+            marginTop: `2rem`,
+          }}
+        >
+          © {new Date().getFullYear()}, Built with
+          {` `}
+          <a href="https://www.gatsbyjs.com">Gatsby</a>
+        </footer>
+    )
+}
+
+export default Projects;
