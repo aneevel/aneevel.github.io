5cb5c8dc5338d9702069a5fe788bb38db5ea0b7f
Alec Neevel <aneevel15@gmail.com>
2021-02-26 02:55:51 -0600

    Wrap button image in an anchor link

--- site/src/components/button.js
+++ site/src/components/button.js
@@ -3,7 +3,9 @@ import React from "react"
 const Button = (props) => {
     return (
         <button id={props.id}>
-            <img src="{props.image}" alt="{props.altText}"/>
+            <a href="{props.link}">
+                <img src="{props.image}" alt="{props.altText}"/>
+            </a>
         </button>
     )
 }
