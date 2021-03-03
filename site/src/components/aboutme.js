dd00bd728a84b15a473af5bf12c535687abb83a4
Alec Neevel <aneevel15@gmail.com>
2021-03-02 20:53:09 -0600

    Add some styling

--- site/src/components/aboutme.js
+++ site/src/components/aboutme.js
@@ -8,7 +8,7 @@ const AboutMe = () => {
             <Image/>
             <h2>A little about me...</h2>
             <p>My name is Alec Neevel. I am web software engineer living in Madison, Wisconsin.
-               I like to work with the full-stack, handling anything from React (which this site is built on!)
+               I like to work with the entire stack, handling anything from React (which this site is built on!)
                to SQL queries.
             </p>
             <Button/>
--- site/src/components/header.js
+++ site/src/components/header.js
@@ -5,7 +5,7 @@ import React from "react"
 const Header = ({ siteTitle }) => (
   <header
     style={{
-      background: `#57836D`,
+      background: `#566954`,
       marginBottom: `1.45rem`,
     }}
   >
