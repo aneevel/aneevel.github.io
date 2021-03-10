cae1567f37516a0b05011981cc397cffc3062d4a
Alec Neevel <aneevel15@gmail.com>
2021-03-10 01:47:45 -0600

    Remove Image (for now)

--- site/src/components/aboutme.js
+++ site/src/components/aboutme.js
@@ -5,7 +5,6 @@ import Button from "./button"
 const AboutMe = () => {
     return (
         <div id="aboutme-container">
-            <Image/>
             <h2>A little about me...</h2>
             <p>My name is Alec Neevel. I am web software engineer living in Madison, Wisconsin.
                I like to work with the entire stack, handling anything from React (which this site is built on!)
