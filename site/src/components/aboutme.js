c0b20b2f67cac9c26fe2bf5180b8e0b1d98b164e
Alec Neevel <aneevel15@gmail.com>
2021-05-20 18:11:19 -0500

    Fill out About Me section

--- site/src/components/aboutme.js
+++ site/src/components/aboutme.js
@@ -5,13 +5,22 @@ import Button from "./button"
 const AboutMe = () => {
     return (
         <div id="aboutme-container">
-            <Image/>
-            <h2>A little about me...</h2>
-            <p>My name is Alec Neevel. I am web software engineer living in Madison, Wisconsin.
+            <h2>Hi! I'm Alec</h2>
+            <p>I'm a web software engineer living in Madison, Wisconsin.
                I like to work with the entire stack, handling anything from React (which this site is built on!)
                to SQL queries.
             </p>
-            <Button/>
+            <p>I design and create websites, web applications, and more. I enjoy using modern technology to build applications 
+                that my clients, family, and friends feel joy using.
+            </p>
+            <p>I've studied Computer Science at the University of Wisconsin - Madison, and Web Software Development at Madison Area Technical College.
+                In 2021, I completed my degree at Madison Area Technical College, and formally entered the world of professional software development.
+            </p>
+            <p>
+                I'm always striving to learn more and put what I learn into practice. I have a sharp, analytical mind with strong
+                creative tendencies that don't quit. If you're interested in learning more about me, or pursuing my efforts
+                for your own technical needs, please read further!
+            </p>
         </div>
     )
 }
