fea22ef7b80bd07e5c1e18230e0c33e91280779f
Alec Neevel <aneevel15@gmail.com>
2021-06-03 16:37:50 -0500

    Bring paragraphs a bit closer together, increase size

--- site/src/components/aboutme.js
+++ site/src/components/aboutme.js
@@ -12,14 +12,41 @@ const AboutMe = () => {
                 className="container flex flex-col">
                 <h2 
                     className="font-bold pb-6 text-4xl"
-                >Hi! I'm Alec</h2>
+                >
+                    Hi! I'm Alec
+                </h2>
                 <p
-                    className="pt-6 pb-6">I'm a web software engineer living in Madison, Wisconsin.
+                    className="pt-6 pb-3 text-xl">I'm a web software engineer living in Madison, Wisconsin.
                 I like to work with the entire stack, handling anything from React (which this site is built on!)
                 to GraphQL.
                 </p>
-                <p>I design, create, and maintain websites, web applications, and more. I enjoy using modern technology to build applications 
+                <p
+                    className="pt-3 pb-3 text-xl"
+                >I design, create, and maintain websites, web applications, and more. I enjoy using modern technology to build applications 
                     that my clients, family, and friends feel joy using.
+                </p>
+                <p
+                    className="pt-3 pb-3 text-xl"
+                >In addition to my web software skills, I have a wide variety of hobbies
+                    and interests. I am always working on developing video games, tackling
+                    every aspect I possibly can, from programming and art to music and design.
+                </p>
+                <p 
+                    className="pt-3 pb-3 text-xl"
+                >
+                    I play guitar, with a particular emphasis on rhythm metal guitar stylings.
+                </p>
+                <p 
+                    className="pt-3 pb-3 text-xl"
+                > 
+                    I am obsessed with baseball and try to support the Milwaukee Brewers as much
+                    as possible.
+                </p>
+                <p 
+                    className="pt-3 pb-3 text-xl"
+                >
+                    Want to chat with me about any of the above? Please feel free to visit my 
+                    Contact section and find a way to get in touch! :)
                 </p>
             </div>
             <div
