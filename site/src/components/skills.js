3462426a26e0e45805a78c95936051ae13342580
Alec Neevel <aneevel15@gmail.com>
2021-06-06 17:19:27 -0500

    Start fleshing out Skills section

--- site/src/components/skills.js
+++ site/src/components/skills.js
@@ -1,8 +1,20 @@
 import React from "react"
 
+import "../styles/skills.css"
+
 const Skills = () => {
     return (
-        <div id="skills-container"></div>
+        <section id="skills-section">
+            <div id="skills-container"
+                className="grid grid-cols-2">
+                <h3 
+                    className="text-2xl font-bold"
+                >
+                    Web Development Skills
+                </h3>
+
+            </div>
+        </section>
     )
 }
 
