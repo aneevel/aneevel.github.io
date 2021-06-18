4896003029bec1588d4a4e6fa9cd1b3b9ddeff6d
aneevel <aneevel15@gmail.com>
2021-06-18 17:57:17 -0500

    Put in the first skill image

--- site/src/components/skills.js
+++ site/src/components/skills.js
@@ -2,10 +2,12 @@ import React from "react"
 
 import "../styles/skills.css"
 
+import angular from "../images/angular.svg";
+
 const Skills = () => {
     return (
         <section id="skills-section"
-            className="pt-10 mr-0 full-width flex flex-col"
+            className="pt-10 mr-0 w-full flex flex-col"
         >
             <h3 
                     className="text-4xl font-bold mx-auto mb-5"
@@ -13,36 +15,95 @@ const Skills = () => {
                     Web Development Skills
                 </h3>
             <div id="skills-container"
-                className="full-width bg-gray-800">
-                <div
-                    className="w-20 h-20"
-                    >
+                className="w-full bg-gray-800"
+            >
+                <div id="skills-grid"
+                    className="grid grid-cols-8 gap-2 items-center"
+                >
+                    <div
+                        className="w-20 h-20 my-4"
+                        >
+                            <img 
+                                alt="Angular"
+                                src={angular}
+                            />
 
-                </div>
-                <div
-                    className="w-20 h-20"
-                    >
+                    </div>
+                    <div
+                        className="w-20 h-20"
+                        >
 
-                </div>
-                <div 
-                    className="w-20 h-20"
-                    >
+                    </div>
+                    <div 
+                        className="w-20 h-20"
+                        >
 
-                </div>
-                <div
-                    className="w-20 h-20"
-                    >
+                    </div>
+                    <div
+                        className="w-20 h-20"
+                        >
 
-                </div>
-                <div
-                    className="w-20 h-20"
-                    >
+                    </div>
+                    <div
+                        className="w-20 h-20"
+                        >
 
-                </div>
-                <div
-                    className="w-20 h-20"
-                    >
+                    </div>
+                    <div
+                        className="w-20 h-20"
+                        >
 
+                    </div>
+                    <div
+                        className="w-20 h-20"
+                        >
+
+                    </div>
+                    <div
+                        className="w-20 h-20"
+                        >
+
+                    </div>
+                    <div
+                        className="w-20 h-20"
+                        >
+
+                    </div>
+                    <div
+                        className="w-20 h-20"
+                        >
+
+                    </div>
+                    <div 
+                        className="w-20 h-20"
+                        >
+
+                    </div>
+                    <div
+                        className="w-20 h-20"
+                        >
+
+                    </div>
+                    <div
+                        className="w-20 h-20"
+                        >
+
+                    </div>
+                    <div
+                        className="w-20 h-20"
+                        >
+
+                    </div>
+                    <div
+                        className="w-20 h-20"
+                        >
+
+                    </div>
+                    <div
+                        className="w-20 h-20"
+                        >
+
+                    </div>
                 </div>
             </div>
         </section>
