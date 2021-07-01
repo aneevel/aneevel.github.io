102ba238dbc06654f7c944ad21846dee0faad9d3
aneevel <aneevel15@gmail.com>
2021-07-01 15:45:04 -0500

    Add gatsby-plugin-image

--- site/src/components/projects.js
+++ site/src/components/projects.js
@@ -1,16 +1,20 @@
 import React from "react"
+import Image from "./image"
 
 const Projects = ({ projects }) => {
     return (
         <section id="projects"
-            className="pt-10 mx-auto w-full flex flex-col">
+            className="pt-10 mx-auto container flex flex-col">
             <h3
                 className="text-4xl font-bold mx-auto mb-5"
                 >
                 Highlighted Work
             </h3>
-            <ul>
-
+            <ul
+                className="grid grid-cols-1 gap-10 md:grid-cols-2 xl:grid-cols-3">
+                {projects.map(project => (
+                    <Project key={project.node.name} project={project.node} />
+                ))}
             </ul>
         </section>
     )
@@ -18,11 +22,17 @@ const Projects = ({ projects }) => {
 
 const Project = ({ project }) => {
     return (
-        <div>
-            <h4>
-                {project.name}
-            </h4>
-        </div>
+        <li>
+            <div 
+                className="bg-purple-200"
+            >
+                <img src={project.coverImg}/>
+                <h4>
+                    {project.name}
+                </h4>
+
+            </div>
+        </li>
     )
 }
 
