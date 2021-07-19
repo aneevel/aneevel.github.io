93c2aa76022cb1590aeb2fad101a80c271f1a3f0
aneevel <aneevel15@gmail.com>
2021-07-19 18:09:15 -0500

    Center intro and remove bodyshot on medium display

--- site/src/components/aboutme.js
+++ site/src/components/aboutme.js
@@ -6,11 +6,11 @@ import bodyshot from '../images/bodyshot.png';
 const AboutMe = () => {
     return (
         <section id="about-me"
-            className="container flex mx-auto pt-32 pl-32 ml-12 float-left"
+            className="container flex mx-auto pt-32 pl-6 float-left md:pt-24"
         >
             <div
                 id="about-me-text"
-                className="w-2/3 pr-8"
+                className="mx-auto pr-8 md:pr-0 md:w-3/4"
             >
                 <h2 
                     className="font-bold pb-6 text-4xl"
@@ -18,45 +18,44 @@ const AboutMe = () => {
                     Hello! My name is <em className="italic text-purple-800">Alec Neevel</em>
                 </h2>
                 <p
-                    className="pt-6 pb-3 text-xl">I'm a web software engineer living in Madison, Wisconsin.
+                    className="pt-6 pb-3 text-xl float-left">I'm a web software engineer living in Madison, Wisconsin.
                 I like to work with the entire stack, handling anything from React (which this site is built on!)
                 to GraphQL.
                 </p>
                 <p
-                    className="pt-3 pb-3 text-xl"
+                    className="pt-3 pb-3 text-xl float-left"
                 >I design, create, and maintain websites, web applications, and more. I enjoy using modern technology to build applications 
                     that my clients, family, and friends feel joy using.
                 </p>
                 <p
-                    className="pt-3 pb-3 text-xl"
+                    className="pt-3 pb-3 text-xl float-left"
                 >In addition to my web software skills, I have a wide variety of hobbies
                     and interests. I am always working on developing video games, tackling
                     every aspect I possibly can, from programming and art to music and design.
                 </p>
                 <p 
-                    className="pt-3 pb-3 text-xl"
+                    className="pt-3 pb-3 text-xl float-left"
                 >
                     I play guitar, with a particular emphasis on rhythm metal guitar stylings.
                 </p>
                 <p 
-                    className="pt-3 pb-3 text-xl"
+                    className="pt-3 pb-3 text-xl float-left"
                 > 
                     I am obsessed with baseball and try to support the Milwaukee Brewers as much
                     as possible.
                 </p>
                 <p 
-                    className="pt-3 pb-3 text-xl"
+                    className="pt-3 pb-3 text-xl float-left"
                 >
                     Want to chat with me about any of the above? Please feel free to visit my 
                     Contact section and find a way to get in touch! :)
                 </p>
             </div>
             <div id="about-me-image"
-                className="overflow-hidden w-1/4 rounded ml-24"
+                className="overflow-hidden rounded pt-12 float-right md:hidden"
             >
                 <img
                     src={bodyshot}
-                    className=""
                     alt="Alec Neevel">
                 </img>
             </div>
