ebc2accd46e4e85840e845d1afbe756301c0a9e0
aneevel <aneevel15@gmail.com>
2025-04-24 15:53:13 -0500

    rename Blog to blog

--- site/src/pages/blog.js
+++ site/src/pages/blog.js
@@ -1,7 +1,7 @@
 import React from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
-import Blog from "../components/Blog"
+import Blog from "../components/blog"
 
 const BlogPage = () => {
   return (
